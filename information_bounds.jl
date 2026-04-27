cec7c3cc35445fb86185a83da8bcc4a3a8727f5d
moises-moran <moises.moran@uj.edu.pl>
2026-04-27 11:58:22 +0800

    projections vs unitaries information_bounds.jl

--- information_bounds.jl
+++ information_bounds.jl
@@ -1,3 +1,4 @@
+# PROJECTORS
 
 # Parameters
 G = 0.8
@@ -18,7 +19,29 @@ tr_eq = [[ρ[x],1] for x in 1:3]
 # Optimization of the semidefinite relaxation
 obj = -(2*PA[1,1]-1)*ρ[1] - (2*PA[1,2]-1)*ρ[1] - (2*PA[1,1]-1)*ρ[2]
 obj+=  (2*PA[1,2]-1)*ρ[2] + (2*PA[1,1]-1)*ρ[3]
-val,_ = npa(obj,2;min=false,
+val, model, _ = npa(obj,2;min=false,
+                 op_ge=op_ge,
+                 tr_eq=tr_eq,
+                 tr_ge=tr_ge,
+                 cyclic=true,
+                 normalize=false)
+println("Optimal value is ", val)
+
+# UNITARIES
+
+G = 0.8
+# Build the monoid
+@pcmonoid M ρ[3,0] σ a[2,0]
+Unipotent.(a)
+build(M)
+# Conditions on the operators
+op_ge = vcat([σ-(1 /3)*r for r in ρ], [r-r^2 for r in ρ]) 
+# Conditions on the moments
+tr_ge = [[-σ,-G]]
+tr_eq = [[ρ[x],1] for x in 1:3]
+# Optimization of the semidefinite relaxation
+obj = -a[1]*ρ[1] - a[2]*ρ[1] - a[1]*ρ[2] +  a[2]*ρ[2] + a[1]*ρ[3]
+val, model, _ = npa(obj,2;min=false,
                  op_ge=op_ge,
                  tr_eq=tr_eq,
                  tr_ge=tr_ge,
