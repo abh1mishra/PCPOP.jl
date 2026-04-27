fef3cb3578757b8c083c58fc79a74e937d9ec5f5
moises-moran <moises.moran@uj.edu.pl>
2026-04-27 12:59:47 +0800

    example triangle network

---
+++ network.jl
@@ -0,0 +1,30 @@
+# Build the monoid
+@pcmonoid M ρ[3,0] a b c
+Projector.([a, b, c])
+@comms a b c
+@comms ρ
+@comms ρ[1] c
+@comms ρ[2] a
+@comms ρ[3] b
+build(M)
+# Objective function.
+r = ρ[1]*ρ[2]*ρ[3]
+p = r*((1-a)*b*c + a*(1-b)*c + a*b*(1-c) - (1-a)*(1-b)*(1-c))
+# Constraints
+S = [ρ[1] - ρ[1]^2,
+	 ρ[2] - ρ[2]^2,
+	 ρ[3] - ρ[3]^2]
+T = [[ρ[1], 1],
+	 [ρ[2], 1],
+	 [ρ[3], 1]]
+# Optimize semidefinite relaxation
+val,model,_ = npa(p,3, min=false,
+					   op_ge = S,
+					   tr_eq = T,
+					   normalize=false,
+					   cyclic=true) 
+println("Termination status ", termination_status(model))
+println("Optimal value is   ", val)
+#sos_model= pcpop(p, 2)
+#println("Termination status ", termination_status(sos_model))
+#println("Optimal value is   ", objective_value(sos_model))
