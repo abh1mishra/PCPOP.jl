1767eb138abbdf0c966417a098a449aee2acbbcf
moises-moran <moises.moran@uj.edu.pl>
2026-04-28 21:33:04 +0800

    implement bilocal network Example 8.1.1 [Klep et al.]

--- bilocal.jl
+++ bilocal.jl
@@ -2,7 +2,7 @@ include("traceGrobner.jl")
 
 # Build the monoid
 @pcmonoid M a[2,0] b[2,0] c[2,0]
-Unipotent.(union([a, b, c]...))
+Unipotent.(M.vertices)
 @comms a b c
 build(M)
 k = 2
@@ -15,8 +15,9 @@ p = (α - γ)^2/8 - (α + γ)
 basis = trace_monomials(TM, 0:k)
 wα = mons_at_level(a, k)
 wγ = mons_at_level(c, k)
-R = [state(u*v, TM) - state(u, TM)*state(v, TM) for u in wα for wb in wγ
-model = tpop(p,basis,equalities=R)
+R = [state(u*v, TM) - state(u, TM)*state(v, TM) for u in wα for v in wγ]
+R = unique([r for r in R if !(r==0)])
+model = tpop(p, TM, basis, equalities=R)
 set_optimizer(model, Mosek.Optimizer)
 optimize!(model)
 println("Termination status ", termination_status(model))
