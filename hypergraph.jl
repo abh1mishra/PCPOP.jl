a8f91ed4eadbf34599d96bf2ff153e88cc0fdc89
moises-moran <moises.moran@uj.edu.pl>
2026-05-12 22:24:17 +0800

    CHSH functional hypergraph scenario

--- hypergraph.jl
+++ hypergraph.jl
@@ -13,6 +13,8 @@ build(M)
 # Objective function
 p = A[2,2] + A[1,3] + A[3,1] + A[1,1]
 p+= A[2,4] + A[4,2] + A[4,3] + A[3,4]
+p+=-A[1,2] - A[1,4] - A[2,1] - A[2,3]
+p+=-A[3,2] - A[3,3] - A[4,1] - A[4,4]
 # Constraints
 R = []
 for i in 1:4
@@ -27,6 +29,14 @@ append!(R, [one(M) - sum(A[3:4,3:4])])
 # Semidefinite relaxation
 println("Building model...")
 op_eq=R
-val,model,_,_ = npa(p, 1, op_eq=R)
+val,model,_,_ = npa(p, 2, min=false, op_eq=R)
+println("Termination status ", termination_status(model))
+println("Optimal value is   ", objective_value(model))
+
+println("Building model...")
+model = pcpop(p, 2, equalities=R)
+set_optimizer(model, Mosek.Optimizer)
+println("Optimizing...")
+optimize!(model)
 println("Termination status ", termination_status(model))
 println("Optimal value is   ", objective_value(model))
