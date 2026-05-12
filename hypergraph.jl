0629f4fff64be3022d0454808b96c0f96c8970c5
moises-moran <moises.moran@uj.edu.pl>
2026-05-12 22:03:43 +0800

    example hypergraph contextuality scenario [!] there are issues

---
+++ hypergraph.jl
@@ -0,0 +1,32 @@
+include("traceGrobner.jl")
+
+println("Building monoid...")
+# Build the monoid
+@pcmonoid M a[16,0]
+Projector.(a)
+A = reshape(a, 4, 4)
+for i in 1:4
+    @comms A[i, :]
+    @comms A[:, i]
+end
+build(M)
+# Objective function
+p = A[2,2] + A[1,3] + A[3,1] + A[1,1]
+p+= A[2,4] + A[4,2] + A[4,3] + A[3,4]
+# Constraints
+R = []
+for i in 1:4
+    append!(R, [one(M) - sum(A[i,:])])
+    append!(R, [one(M) - sum(A[:,i])])
+end
+append!(R, [one(M) - sum(A[1:2,1:2])])
+append!(R, [one(M) - sum(A[1:2,3:4])])
+append!(R, [one(M) - sum(A[3:4,1:2])])
+append!(R, [one(M) - sum(A[3:4,3:4])])
+
+# Semidefinite relaxation
+println("Building model...")
+op_eq=R
+val,model,_,_ = npa(p, 1, op_eq=R)
+println("Termination status ", termination_status(model))
+println("Optimal value is   ", objective_value(model))
