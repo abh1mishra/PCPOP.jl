83eeb7ce8ae8252c692ab9bccc637a636e5524f9
moises-moran <moises.moran@uj.edu.pl>
2026-06-29 23:06:20 +0800

    test mermin in bilocal network

---
+++ network-bilocal.jl
@@ -0,0 +1,29 @@
+include("traceGrobner.jl")
+
+# Build the monoid
+@pcmonoid M ρ[2,0] a[2,0] b[2,0] c[2,0]
+Unipotent.([a b c])
+Projector.(ρ)
+@comms a b c
+@comms ρ
+@comms ρ[1] c
+@comms ρ[2] a
+#@comms M.vertices...
+build(M)
+# Objective function.
+σ = ρ[1]*ρ[2]
+p = σ*(a[2]*b[1]*c[1] + a[1]*b[2]*c[1] + a[1]*b[1]*c[2] - a[2]*b[2]*c[2])
+# Constraints
+S = []
+T = [[σ, 1],
+     [ρ[1]-ρ[2], 0]]
+
+# Optimize semidefinite relaxation
+val,model,_ = pcpop!(p,3, min=false,
+					   op_ge = S,
+					   tr_eq = T,
+					   normalize=false,
+					   tracial=true,
+                       lvl_lm=1) 
+println("Termination status ", termination_status(model))
+println("Optimal value is   ", val)
